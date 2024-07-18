49d476fa311e15e526bc65c6194d61786ff359e9
lemika1243 <lemikajy@gmail.com>
2024-07-18 20:47:49 +0300

    vita ilay camera tompoko

---
+++ assets/tilesets/Terrain (16x16).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="Landscapes" tilewidth="32" tileheight="32" tilecount="55" columns="11">
+ <image source="Terrain (16x16).png" width="352" height="176"/>
+</tileset>
