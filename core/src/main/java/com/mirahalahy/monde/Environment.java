b09da17ea78ce7b8a90525f828a8bbe18d630056
lemika1243 <lemikajy@gmail.com>
2024-07-22 00:04:06 +0300

    vita ny GameObject class miaraka

--- core/src/main/java/com/mirahalahy/monde/Environment.java
+++ core/src/main/java/com/mirahalahy/monde/Environment.java
@@ -1,5 +1,7 @@
 package com.mirahalahy.monde;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
@@ -14,6 +16,7 @@ import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.mirahalahy.objet.GameObject;
 
 public class Environment {
     int id;
@@ -21,19 +24,33 @@ public class Environment {
     OrthogonalTiledMapRenderer tmr;
     World world;
     Box2DDebugRenderer rendererBox2d;
+    GameObject player;
 
     public Environment(int id) {
         map = new TmxMapLoader().load("maps/level1.tmx");
         initWorld();
         createWorld();
+        player = new GameObject(world, 28, 28);
+        player.setPosition(new Vector2(0, 100));
+        player.setImage("images/player/Idle.png", 11);
+        player.createBody();
+        player.setVitesse(new Vector2(0, -250));
+    }
+
+    public void handleInput() {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            Camera.getCamera().position.x += 1;
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+            Camera.getCamera().position.x -= 1;
+        }
     }
 
     public void initWorld() {
         tmr = new OrthogonalTiledMapRenderer(map);
         Camera.setCamera(new OrthographicCamera());
-        world = new World(new Vector2(0, 0), true);
+        world = new World(new Vector2(0, -10), true);
         rendererBox2d = new Box2DDebugRenderer();
-        Camera.getCamera().position.set(Camera.getView().getWorldWidth() / 2, Camera.getView().getWorldHeight() / 2, 0);
     }
 
     public void createWorld() {
@@ -62,13 +79,12 @@ public class Environment {
     }
 
     public void render() {
+        handleInput();
         Camera.update();
         tmr.setView(Camera.getCamera());
         tmr.render();
         rendererBox2d.render(world, Camera.getCombined());
-    }
-
-    public void resize(int width, int height) {
-        Camera.resize(width, height);
+        world.step(1/60f, 6, 2);
+        player.renderImage();
     }
 }
